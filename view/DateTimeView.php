d72546bd9f66dcbed70b1f1a5a1e716ca7c5c9e4
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-09-19 13:42:01 +0200

    2

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -7,8 +7,10 @@ class DateTimeView {
 
 		$date = getdate();
 
-		$timeString = "$date[weekday], the $date[mday]th of $date[month] $date[year], The Time is $date[hours]:$date[minutes]";
+		// $timeString = "$date[weekday], the $date[mday]th of $date[month] $date[year], The Time is $date[hours]:$date[minutes]";
 		$time =  '$date[hours]:$date[minutes]';
+
+		$timeString = 'Wednesday, the 19th of September 2018, The time is '
 
 		return '<p>' . $timeString . '</p>';
 	}
