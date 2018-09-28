1c9f4638d52c00cc6e87916391fad78636b7302e
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-09-28 22:19:12 +0200

    final

--- controller/ViewController.php
+++ controller/ViewController.php
@@ -121,7 +121,6 @@ require_once('model/UserDbAuthenticator.php');
 */
 		private function redirect($username) {
 			header("Location:/?username=$username&message=registersucess");
-			unset($_GET["register"]);
 		}
 
 		private function sessionHandler () {
