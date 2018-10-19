25661affbb15901c9025f5a522083d7778d72498
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-10-19 13:35:12 +0200

    calendar model

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ require_once('model/MySqlDataBase.php');
 require_once('model/User.php');
 require_once('controller/LogInHandler.php');
 //require_once('controller/Navigator.php');
-//require_once('controller/calenderHandler.php');
+require_once('controller/calenderHandler.php');
 require_once('model/LogInPercistency.php');
 
-//require_once('model/Calendar/Calendar.php');
-//require_once('model/Calendar/CalendarSettings.php');
+require_once('model/Calendar/Calendar.php');
+require_once('model/Calendar/CalendarSettings.php');
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
@@ -43,17 +43,17 @@ $dtv = new DateTimeView();
 $lv = new LayoutView($dtv);
 $ehv = new ExceptionHandlerView();
 $rv = new RegisterView();
-//$calendarView = new CalendarView();
+$calendarView = new CalendarView();
 
 
 //create Claendar
-//$cal =  new Model\Calendar\Calendar();
+$cal =  new Model\Calendar\Calendar();
 //$calSett = new Model\Calendar\CalendarSettings();
 
 
 //CREATE CONTROLLER
 $c = new LogInHandler($v, $lv, $SqlLogInDatabase, $ehv, $rv);
-//$calenderHandler = new Controller\Calender\CalenderHandler($calendarView);
+$calenderHandler = new Controller\Calender\CalenderHandler($calendarView);
 //$navigator = new Controller\Navigator($lv,$calenderHandler , $c);
 
 $c->startLogInHandler();
