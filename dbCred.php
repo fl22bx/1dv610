ec55a487d649bd12fcc55471af6c29ce1df6c121
Fredrik Stenmark <fl22bx@student.lnu.se>
2018-10-21 23:08:25 +0200

    public

---
+++ dbCred.php
@@ -0,0 +1,22 @@
+<?php
+
+ class dbCred
+{
+
+	public function dbServername() : string {
+		return "localhost";
+	}
+
+	public function dbUsername() : string {
+		return "fredrik";
+	}
+
+	public function dbPassword() : string {
+		return "test";
+	}
+
+	public function dbName() : string {
+		return "user";
+	}
+
+}
